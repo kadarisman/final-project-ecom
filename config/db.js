8a9c89c46e8359582e213ad3bf4a93bbac793c39
kadarisman <yuzarqadariesman@gmail.com>
2022-06-17 00:31:47 +0700

    update password dari planetscale akun

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const config = {
     host :"dif4vod2f1e2.us-east-1.psdb.cloud",
-    user:"rzja71vwmtnf",
-    password: "pscale_pw_BGf9qnEHDlJrOwgn3vfzS3sj5x5GRrZn47VSBjD_lk4",
+    user:"j2w6pf1x5zjl",
+    password: "pscale_pw_7uDg8hA8uQDtz0QJh9PjHzgbs2g0uO6ePius05LkLe0",
     database:"ecom_1",
     ssl:{
         "rejectUnauthorized":true
