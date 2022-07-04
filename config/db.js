50e2f6f1a47e77c74f6c967096017a4476c32677
kadarisman <yuzarqadariesman@gmail.com>
2022-07-04 14:10:26 +0700

    change database server

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 const config = {
-    host :"dif4vod2f1e2.us-east-1.psdb.cloud",
-    user:"j2w6pf1x5zjl",
+    host :"sql6.freesqldatabase.com",
+    user:"sql6503878",
     password: "pscale_pw_7uDg8hA8uQDtz0QJh9PjHzgbs2g0uO6ePius05LkLe0",
-    database:"ecom_1",
-    ssl:{
-        "rejectUnauthorized":true
-    }
+    database:"QMXLVKgQMC",
 }
 
 module.exports = config;
