3efc9f293c182db25b5dd752d5a765cc6b4fdf2c
kadarisman <yuzarqadariesman@gmail.com>
2022-07-04 14:30:13 +0700

    fix db config

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const config = {
     host :"sql6.freesqldatabase.com",
     user:"sql6503878",
-    password: "pscale_pw_7uDg8hA8uQDtz0QJh9PjHzgbs2g0uO6ePius05LkLe0",
-    database:"QMXLVKgQMC",
+    password: "QMXLVKgQMC",
+    database:"sql6503878",
 }
 
 module.exports = config;
